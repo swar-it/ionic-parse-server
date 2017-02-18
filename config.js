abee90d687d4091622469813aca7994ab4ee24d0
Swarit Agarwal <agarwalswarit@gmail.com>
2017-02-18 13:04:32 +0800

    set local configuration

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-/*module.exports = {
-  server: {},
-  dashboard: {},
-  storage: {},
-  push: {}
-}*/
-
 module.exports = {
+	server: {},
+	dashboard: {},
+	storage: {},
+	push: {}
+}
+
+/*module.exports = {
 	server: {
 		databaseURI: 'mongodb://ionicparseserverdb-parse:cy3GiYWZLzyQavSkVTGGYWppYKW5Gxcicv0JdGhDLfOi1KG4tCq9sIx83Bdx5nwAvh3o0bOA0SInTco2NYmgHQ==@ionicparseserverdb-parse.documents.azure.com:10250/parse?ssl=true', // Connection string for your MongoDB database
 		cloud: '/Users/swaritkagarwal/Documents/DEV/ionic-parse-server/cloud/main.js', 
@@ -27,4 +27,4 @@ module.exports = {
     },    
 	storage: {},
 	push: {}
-}
+}*/
